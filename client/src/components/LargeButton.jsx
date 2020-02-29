58a76b1cbf98415d9dfde10863206e0f853565e2
Josh Diamond <diamondroad91@gmail.com>
2020-02-29 08:58:02 -0700

    clear-button-styles

--- client/src/components/LargeButton.jsx
+++ client/src/components/LargeButton.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import clear from '../static/clear.png'
 
 export default function LargeButton({ symbol, update }) {
     return (
-        <div className='large-button' onClick={()=> update(symbol)}>
-            <div className='large-diamond'>
+        // <div className='large-button' onClick={()=> update(symbol)}>
+        //     <div className='large-diamond'>
 
-            </div>
-            <div className='large-diamond-2'>
+        //     </div>
+        //     <div className='large-diamond-2'>
                     
-            </div>
-            <div className='large-rectangle'>
-                {/* <h4>{symbol}</h4> */}
-            </div>
-            <div className='large-button-symbol'>
-                <h4 className='clear'>CLEAR</h4>
-            </div>
+        //     </div>
+        //     <div className='large-rectangle'>
+        //         {/* <h4>{symbol}</h4> */}
+        //     </div>
+        //     <div className='large-button-symbol'>
+        //         <h4 className='clear'>CLEAR</h4>
+        //     </div>
+        // </div>
+        <div className='clear-button-fix' onClick={()=> update(symbol)}>
+            <img src={clear} />
         </div>
     )
 }
